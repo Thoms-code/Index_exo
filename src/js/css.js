52bc8c1077f71fe90e56a5745e5ced28fe6cf450
thomas <catalanothomas@outlook.fr>
2019-12-13 19:34:33 +0100

    version 0.1 build

---
+++ src/js/css.js
@@ -0,0 +1,5 @@
+let button_developper = document.getElementById("button_developper");
+
+button_developper.addEventListener("hover" , () => {
+    button_developper.style.backgroundColor = "white";
+})
--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,15 @@
+let clicHTML = document.getElementsByClassName("container_menu_sommaire")[0];
 let clicCSS = document.getElementsByClassName("container_menu_sommaire")[1];
+let clicJavascript = document.getElementsByClassName("container_menu_sommaire")[2];
+console.log(clicJavascript);
 
+
+clicHTML.addEventListener("click" , () => {
+    window.open("../../pages/html.html" , "_self")
+})
 clicCSS.addEventListener("click" , () => {
-    window.open("../../pages/css.html" , "_blank")
+    window.open("../../pages/css.html" , "_self")
+})
+clicJavascript.addEventListener("click" , () => {
+    window.open("../../pages/javascript.html" , "_self")
 })
