863700cb849c334b568ce82f5b437ce486409234
thomas <catalanothomas@outlook.fr>
2019-12-14 18:41:06 +0100

    Version 0.2

--- src/js/css.js
+++ src/js/css.js
@@ -1,5 +1,19 @@
-let button_developper = document.getElementById("button_developper");
-
-button_developper.addEventListener("hover" , () => {
-    button_developper.style.backgroundColor = "white";
+/* ANIMATION */
+let buttonOuvrirAnimation = document.getElementById("button_ouvrir_animation");
+let buttonFermerAnimation = document.getElementById("button_fermer_animation");
+let toggleGrandAnimation = document.getElementsByClassName("toggle_grand_animation")[0];
+let togglePetitAnimation = document.getElementsByClassName("toggle_petit_animation")[0];
+let containerSoloAnimation = document.getElementsByClassName("container_solo_css_animation")[0];
+buttonOuvrirAnimation.addEventListener("click" , () => {
+    containerSoloAnimation.classList.toggle("toggle");
+    togglePetitAnimation.setAttribute("id" , "display_none");
+    toggleGrandAnimation.id = "";
 })
+buttonFermerAnimation.addEventListener("click" , () => {
+    containerSoloAnimation.classList.toggle("toggle");
+    toggleGrandAnimation.id = "display_none";
+    togglePetitAnimation.id = "";
+})
+/* FIN ANIMATION */
+
+/* SELECTEURS */
